16c15a46dc540f9b0fb3316ecec74bd2b4d7b123
soltonbaev <isoltonbaev@gmail.com>
2023-03-15 14:58:39 +0600

    .

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,10 +4,11 @@ import discordLogo from './media/DiscordLogo.svg';
 import ytLogo from './media/YoutubeLogo.svg';
 import twitterLogo from './media/TwitterLogo.svg';
 import instagramLogo from './media/InstagramLogo.svg';
+import divider from './media/Divider.svg';
 
 const Footer = () => {
    return (
-      <footer className="container flex mx-auto items-center justify-evenly px-10 ">
+      <footer className="container  mx-auto items-center px-10 ">
          <div className="footer-top grid sm:grid-cols-1 lg:grid-cols-3 gap-4">
             <div className="column1">
                <div className="flex">
@@ -33,16 +34,21 @@ const Footer = () => {
             <div className="column3">
                <h3>Join our weekly digest</h3>
                <p>Get exclusive promotions & updates straight to your inbox.</p>
-               <div>
+               <div className="flex flex-col md:flex-row gap-y-4 relative">
                   <input
                      className="inp"
                      placeholder="enter your email here"
                   ></input>
-                  <button>Subscribe</button>
+                  <button className="md:absolute md:left-[190px]">
+                     Subscribe
+                  </button>
                </div>
             </div>
          </div>
-         <div className="footer-bottom"></div>
+         <div className="border-b pt-10"></div>
+         <div className="footer-bottom py-2">
+            &copy; layout crafted by Ibraim Soltonbaev 2023
+         </div>
       </footer>
    );
 };
