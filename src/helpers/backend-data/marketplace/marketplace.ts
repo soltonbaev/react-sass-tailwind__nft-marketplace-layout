62ccc18806bd3f62e9ba984e4f4a6bfb764875fe
soltonbaev <isoltonbaev@gmail.com>
2023-03-18 09:17:03 +0600

    .

---
+++ src/helpers/backend-data/marketplace/marketplace.ts
@@ -0,0 +1,136 @@
+import image1 from './Image Placeholder.png';
+import image2 from './Image Placeholder-1.png';
+import image3 from './Image Placeholder-2.png';
+import image4 from './Image Placeholder-3.png';
+import image5 from './Image Placeholder-4.png';
+import image6 from './Image Placeholder-5.png';
+import image7 from './Image Placeholder-6.png';
+import image8 from './Image Placeholder-7.png';
+import image9 from './Image Placeholder-8.png';
+import image10 from './Image Placeholder-9.png';
+import image11 from './Image Placeholder-10.png';
+import image12 from './Image Placeholder-11.png';
+
+import avatar1 from './Avatar Placeholder.png';
+import avatar2 from './Avatar Placeholder-1.png';
+import avatar3 from './Avatar Placeholder-2.png';
+import avatar4 from './Avatar Placeholder-3.png';
+import avatar5 from './Avatar Placeholder-4.png';
+import avatar6 from './Avatar Placeholder-5.png';
+import avatar7 from './Avatar Placeholder-6.png';
+import avatar8 from './Avatar Placeholder-7.png';
+import avatar9 from './Avatar Placeholder-8.png';
+import avatar10 from './Avatar Placeholder-9.png';
+import avatar11 from './Avatar Placeholder-10.png';
+import avatar12 from './Avatar Placeholder-11.png';
+
+export const marketplaceNFTs = [
+   {
+      image: image1,
+      title: '',
+      creatorPic: avatar1,
+      creatorName: '',
+      price: 1.75,
+      highestBid: 0.55,
+      id: 1,
+   },
+   {
+      image: image2,
+      title: '',
+      creatorPic: avatar2,
+      creatorName: '',
+      price: 1.75,
+      highestBid: 0.55,
+      id: 2,
+   },
+   {
+      image: image3,
+      title: '',
+      creatorPic: avatar3,
+      creatorName: '',
+      price: 1.75,
+      highestBid: 0.55,
+      id: 3,
+   },
+   {
+      image: image4,
+      title: '',
+      creatorPic: avatar4,
+      creatorName: '',
+      price: 1.75,
+      highestBid: 0.55,
+      id: 4,
+   },
+   {
+      image: image5,
+      title: '',
+      creatorPic: avatar5,
+      creatorName: '',
+      price: 1.75,
+      highestBid: 0.55,
+      id: 5,
+   },
+   {
+      image: image6,
+      title: '',
+      creatorPic: avatar6,
+      creatorName: '',
+      price: 1.75,
+      highestBid: 0.55,
+      id: 6,
+   },
+   {
+      image: image7,
+      title: '',
+      creatorPic: avatar7,
+      creatorName: '',
+      price: 1.75,
+      highestBid: 0.55,
+      id: 7,
+   },
+   {
+      image: image8,
+      title: '',
+      creatorPic: avatar8,
+      creatorName: '',
+      price: 1.75,
+      highestBid: 0.55,
+      id: 8,
+   },
+   {
+      image: image9,
+      title: '',
+      creatorPic: avatar9,
+      creatorName: '',
+      price: 1.75,
+      highestBid: 0.55,
+      id: 9,
+   },
+   {
+      image: image10,
+      title: '',
+      creatorPic: avatar10,
+      creatorName: '',
+      price: 1.75,
+      highestBid: 0.55,
+      id: 10,
+   },
+   {
+      image: image11,
+      title: '',
+      creatorPic: avatar11,
+      creatorName: '',
+      price: 1.75,
+      highestBid: 0.55,
+      id: 11,
+   },
+   {
+      image: image12,
+      title: '',
+      creatorPic: avatar12,
+      creatorName: '',
+      price: 1.75,
+      highestBid: 0.55,
+      id: 12,
+   },
+];
