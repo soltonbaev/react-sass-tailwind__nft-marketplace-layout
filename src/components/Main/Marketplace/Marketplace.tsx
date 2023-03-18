438f6de5258e08dad652e7a4337913e7f6f1d227
soltonbaev <isoltonbaev@gmail.com>
2023-03-18 09:18:49 +0600

    .

--- src/components/Main/Marketplace/Marketplace.tsx
+++ src/components/Main/Marketplace/Marketplace.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import MoreFromMarketplace from './MoreFromMarketplace';
 
 const Marketplace = () => {
-   return <div></div>;
+   return (
+      <main className="marketplace">
+         <div className="artist__tabs border-t border-[#3b3b3b] ">
+            <div className="tab__btns container px-10 mx-auto items-center grid grid-cols-2 ">
+               <div className="flex items-center justify-center gap-4 border-b p-[1.5rem]">
+                  <span className="font-bold">NFTs</span>
+                  <span className="rounded-[1.25rem] px-[10px] py-[5px] bg-[#858584]">
+                     302
+                  </span>
+               </div>
+               <div className="flex items-center justify-center gap-4 p-[1.5rem]">
+                  <span className="text-[#858584] font-bold">Collections</span>
+                  <span className="rounded-[1.25rem] px-[10px] py-[5px] bg-secondary">
+                     67
+                  </span>
+               </div>
+            </div>
+         </div>
+         <MoreFromMarketplace />
+      </main>
+   );
 };
 
 export default Marketplace;
